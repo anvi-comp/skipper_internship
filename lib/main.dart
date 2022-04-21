e5591d99f2444ea5eefd8fdad06c27cf95154e80
anvi-comp <67826061+anvi-comp@users.noreply.github.com>
2022-04-22 00:18:20 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:bs_flutter_datatable/bs_flutter_datatable.dart';
+
+import 'datasource/employee_datasource.dart';
+import 'model/employee.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,179 +14,114 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Form Widgets',
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: MyHomePage(title: 'SfDataGrid Editing'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
+  late EmployeeDataSource _employeeDataSource;
+  List<Employee> _employees = <Employee>[];
+  late DataGridController _dataGridController;
 
-
-  String dropdownValue = 'Dept1';
-  String dropdownValue2 = 'Active';
-
+  @override
+  void initState() {
+    super.initState();
+    _employees = getEmployeeData();
+    _employeeDataSource = EmployeeDataSource(_employees);
+    _dataGridController = DataGridController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Designation'),
-        ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Container(
-              padding: EdgeInsets.only(top: 10.0, bottom: 5.0, left: 50.0, right: 50.0),
-              child: Column(children: [
-                Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                  Container(
-                    color: Colors.lightGreenAccent,
-                    child: RawMaterialButton(
-                      child: Text('View Details', style: TextStyle(color: Colors.black)),
-                      onPressed: () {
-                        //Do Something
-                      },
-                    ),
-                  )
-                ]),
-                //Text('TextFormField'),
-                SizedBox(height: 10.0),
-                TextFormField(
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                      hintText: 'Department Name',
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey, width: 32.0),
-                          borderRadius: BorderRadius.circular(5.0)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                          borderRadius: BorderRadius.circular(5.0)
-                      )
-                  ),
-                  onChanged: (value) {
-                    //Do something with this value
-                  },
-                ),
-
-                //Text('TextFormField'),
-                SizedBox(height: 10.0),
-                TextFormField(
-                  keyboardType: TextInputType.text,
-                  maxLines: 4,
-                  decoration: InputDecoration(
-                      hintText: 'Description',
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey, width: 32.0),
-                          borderRadius: BorderRadius.circular(5.0)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                          borderRadius: BorderRadius.circular(5.0)
-                      )
-
-                  ),
-                  onChanged: (value) {
-                    //Do something with this value
-                  },
-                ),
-                SizedBox(height: 10.0),
-
-                SizedBox(height: 10.0),
-                //Text('DropDown Button'),
-                SizedBox(height: 10.0),
-
-
-                  Container(
-                  padding: EdgeInsets.only(left: 5.0, right: 5.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(3.0)
-                  ),
-                  child: DropdownButton<String>(
-                    //value: dropdownValue,
-                    isExpanded: true,
-                    hint: Text("Department"),
-                    icon: Icon(Icons.keyboard_arrow_down, size: 22),
-                    underline: SizedBox(),
-                    items: <String>['Dept1', 'Dept2', 'Dept3', 'Dept4'].map((String value) {
-                      return new DropdownMenuItem<String>(
-                        value: value,
-                        child: new Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (value) {
-                      //Do something with this value
-                      setState(() {
-                        dropdownValue = value!;
-                      });
-                    },
-                  ),
-                ),
-                SizedBox(height: 10.0),
-
-                SizedBox(height: 10.0),
-                //Text('DropDown Button'),
-                SizedBox(height: 10.0),
-
-
-                Container(
-                  padding: EdgeInsets.only(left: 5.0, right: 5.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(3.0)
-                  ),
-                  child: DropdownButton<String>(
-                    //value: dropdownValue2,
-                    hint: Text("Status"),
-                    isExpanded: true,
-                    icon: Icon(Icons.keyboard_arrow_down, size: 22),
-                    underline: SizedBox(),
-                    items: <String>['Active','Not-Active'].map((String value) {
-                      return new DropdownMenuItem<String>(
-                        value: value,
-                        child: new Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (value) {
-                      //Do something with this value
-                      setState(() {
-                        dropdownValue = value!;
-                      });
-                    },
-                  ),
-                ),
-
-
-
-
-                SizedBox(height: 10.0),
-                //Text('Buttons'),
-                SizedBox(height: 10.0),
-                //Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-
-                  Container(
-                    color: Colors.lightBlue,
-                    child: RawMaterialButton(
-                      child: Text('Submit', style: TextStyle(color: Colors.white)),
-                      onPressed: () {
-                        //Do Something
-                      },
-                    ),
-                  )
-                //])
-              ])
+      appBar: AppBar(
+        title: Text('SfDataGrid Editing'),
+      ),
+      body: SfDataGrid(
+        source: _employeeDataSource,
+        allowEditing: true,
+        selectionMode: SelectionMode.single,
+        navigationMode: GridNavigationMode.cell,
+        columnWidthMode: ColumnWidthMode.fill,
+        controller: _dataGridController,
+        columns: [
+          GridColumn(
+            columnName: 'id',
+            label: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              alignment: Alignment.centerRight,
+              child: Text(
+                'ID',
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
           ),
-        )
+          GridColumn(
+            columnName: 'name',
+            label: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Name',
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+          GridColumn(
+            columnName: 'designation',
+            label: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Designation',
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+          GridColumn(
+            columnName: 'salary',
+            label: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              alignment: Alignment.centerRight,
+              child: Text(
+                'Salary',
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+
+  List<Employee> getEmployeeData() {
+    return [
+      Employee(10001, 'James', 'Project Lead', 20000),
+      Employee(10002, 'James', 'Manager', 30000),
+      Employee(10003, 'James', 'Developer', 15000),
+      Employee(10004, 'James', 'Developer', 15000),
+      Employee(10005, 'James', 'Developer', 15000),
+      Employee(10006, 'James', 'Developer', 15000),
+      Employee(10007, 'James', 'Developer', 15000),
+      Employee(10008, 'James', 'Developer', 15000),
+      Employee(10009, 'James', 'Developer', 15000),
+      Employee(10010, 'James', 'Developer', 15000)
+    ];
   }
 }
